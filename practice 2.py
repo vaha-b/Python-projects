f41bf70fd3396c90af0786db2c038a892b1803ee
Vahidin <55129308+vaha-b@users.noreply.github.com>
2021-05-19 16:35:26 +0200

    Update practice 2.py

--- practice 2.py
+++ practice 2.py
@@ -23,40 +23,7 @@ for river in River.all_rivers:
 # def __str__(self):
 #        return '{} by {}. ${}. [{}]'.format(self.title, self.author, self.price, self.book_id)
 
-# def new_account():
-#     randint = str(random.randint(0000000000, 9999999999))
-#     rand_num = str(random.randint(0000, 9999))
 
-#     while len(randint) != 10:
-#         num = randint
-#         randint = str(random.randint(0000000000, 9999999999))
-#     while len(rand_num) != 4:
-#         pin = rand_num
-#         rand_num = str(random.randint(0000, 9999))
-
-#     checksum = num[-1]
-#     BIN = num[:-2:-1]
-
-#     num = "400000" + BIN
-#     num_list = []
-#     for i in num:
-#         if int(i) * 2 <= 9 and i in num[::2]:
-#             num_list.append(int(i) * 2)
-#         elif int(i) * 2 <= 9 and i in num[::2]:
-#             num_list.append(str((int(i) * 2) - 9))
-#         else:
-#             num_list.append(i)
-
-#     return num_list + [checksum]
-
-
-# def final():
-#     while sum(map(int, new_account())) % 10 != 0:
-#         credit_card = sum(map(int, new_account()))
-#     print(credit_card)
-
-
-# final()
 
 # with open("University Admission Procedure\Stage 4 - Applicant_list_example.txt", "r") as f:
 #     applicants = []
